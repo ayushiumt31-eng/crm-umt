5390c48b706e072b7dcca0727f4840fa4e6815f1
ayushiumt31-eng <ayushiumt31@gmail.com>
2026-07-17 15:55:36 +0530

    Complete sidebar groups and navigation

--- frontend/src/components/layout/Sidebar/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar/Sidebar.tsx
@@ -1,12 +1,16 @@
 import SidebarLogo from "./SidebarLogo";
+import SidebarGroup from "./SidebarGroup";
+import { sidebarMenu } from "@/constants/sidebarMenu";
 
 export default function Sidebar() {
   return (
-    <aside className="flex h-screen w-64 flex-col border-r border-gray-200 bg-white">
+    <aside className="flex h-screen w-64 flex-col border-r bg-white">
       <SidebarLogo />
 
-      <div className="flex-1 p-4">
-        Sidebar Menu
+      <div className="flex-1 overflow-y-auto p-4">
+        {sidebarMenu.map((group) => (
+          <SidebarGroup key={group.id} group={group} />
+        ))}
       </div>
     </aside>
   );

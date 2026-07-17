f266b56f17cfacd0d6a3c4cc72d1902a34327983
ayushiumt31-eng <ayushiumt31@gmail.com>
2026-07-17 16:02:37 +0530

    Complete sidebar with profile section

--- frontend/src/components/layout/Sidebar/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar/Sidebar.tsx
@@ -1,17 +1,23 @@
 import SidebarLogo from "./SidebarLogo";
 import SidebarGroup from "./SidebarGroup";
 import { sidebarMenu } from "@/constants/sidebarMenu";
+import SidebarProfile from "./SidebarProfile";
 
 export default function Sidebar() {
   return (
     <aside className="flex h-screen w-64 flex-col border-r bg-white">
       <SidebarLogo />
 
-      <div className="flex-1 overflow-y-auto p-4">
-        {sidebarMenu.map((group) => (
-          <SidebarGroup key={group.id} group={group} />
-        ))}
-      </div>
+      
+
+        <div className="flex-1 overflow-y-auto p-4">
+          {sidebarMenu.map((group) => (
+            <SidebarGroup key={group.id} group={group} />
+          ))}
+        </div>
+
+        <SidebarProfile />
+      
     </aside>
   );
 }

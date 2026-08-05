bfa23615de0331d5933449d71a42992f5ed1056b
ayushiumt31-eng <ayushiumt31@gmail.com>
2026-08-05 11:02:39 +0530

    setup backend foundation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CRM API')
+    .setDescription('Enterprise CRM Backend API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+
+  console.log(
+    `🚀 Server Running : http://localhost:${process.env.PORT ?? 3000}`,
+  );
+
+  console.log(
+    `📚 Swagger Docs : http://localhost:${process.env.PORT ?? 3000}/api`,
+  );
 }
+
 bootstrap();
